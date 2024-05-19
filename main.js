0b00c6e5095cadf5ff162468da6d7753895dd28f
Mohammad Abu Huraira Saim <88364296+ahsaim04@users.noreply.github.com>
2024-05-19 22:03:29 +0600

    ss

---
+++ main.js
@@ -0,0 +1,9 @@
+const baseUrl = `https://todo-crudl.deno.dev/saim/todos`;
+const  AddTask = document.getElementById('add-task-button');
+
+AddTask.addEventListener('click', async()=> {
+    
+
+
+
+})
